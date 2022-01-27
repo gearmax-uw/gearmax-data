13870dd202732f9b8ae2d4d4630059ffadbcab49
Lucasf1-uw <l36qiu@uwaterloo.ca>
2022-01-26 21:42:35 -0500

    change values from varchar to boolean

--- builder/depreciation_info.sql
+++ builder/depreciation_info.sql
@@ -1,15 +1,15 @@
 DROP TABLE IF EXISTS depreciation_info;
-
+  
 select '-----------------------------------------------------------------' as '';
 select 'Create depreciation_info' as '';
 
 create table if not exists depreciation_info(
-			id int NOT NULL primary key,
-            frame_damaged varchar(5),
-            has_accidents varchar(5),
-            salvage varchar(5),
-            isCab varchar(5),
-            theft_title varchar(5)
+            id int NOT NULL primary key,
+            frame_damaged boolean,
+            has_accidents boolean,
+            salvage boolean,
+            is_cab boolean,
+            theft_title boolean
             );
 
 load data infile 'test_data/depreciation_info.csv' ignore into table depreciation_info
@@ -17,5 +17,10 @@ load data infile 'test_data/depreciation_info.csv' ignore into table depreciatio
     fields terminated by ','
     enclosed by '"'
     lines terminated by '\n'
-    ignore 1 lines 
-
+    ignore 1 lines
+    (id, @frame_damaged, @has_accidents, @salvage, @is_cab, @theft_title)
+    set frame_damaged = (@frame_damaged = 'True'),
+        has_accidents = (@has_accidents = 'True'),
+        salvage = (@salvage = 'True'),
+        is_cab = (@is_cab = 'True'),
+        theft_title = (@theft_title = 'True');
