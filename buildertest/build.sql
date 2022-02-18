689b245cbeeace7f273f76acaa349b468d81633d
Lucasf1-uw <l36qiu@uwaterloo.ca>
2022-02-17 23:22:22 -0500

    remove test builder files

--- buildertest/build.sql
+++
@@ -1,9 +0,0 @@
-Drop Database if exists UsedCarDebug;
-Create Database UsedCarDebug;
-use UsedCarDebug;
-source car.sql
-source depreciation_info.sql
-source user.sql
-source pickup_truck.sql
-source car_option.sql
-source major_option.sql
